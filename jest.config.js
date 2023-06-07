c9da2f3069ba6538a130497874f18cf77547f1c7
Mahabub Alam <mahabubx7@gmail.com>
2023-06-07 11:08:12 +0600

    :rocket: Setup(core):added-exts-tiny-conf

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+const { compilerOptions } = require("./tsconfig.json");
+
+const pathsToModuleNameMapper = (paths, { prefix }) => {
+  const aliases = {};
+
+  Object.keys(paths).forEach((item) => {
+    const key = item.replace("/*", "/(.*)");
+    const value = `${prefix}${paths[item][0].replace("/*", "/$1")}`;
+    aliases[key] = value;
+  });
+
+  return aliases;
+};
+
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
+  transform: {
+    "^.+\\.ts?$": "ts-jest",
+  },
+  testRegex: "/tests/.*\\.(test|spec)\\.ts?$",
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+};
