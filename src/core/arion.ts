180e9b4ccf93b19a191cb7c63436ccde8447a919
Mahabub Alam <mahabubx7@gmail.com>
2023-06-07 11:08:52 +0600

    :rocket: Setup(core):added-src-arionjs-hello-world

---
+++ src/core/arion.ts
@@ -0,0 +1,17 @@
+import express, { Express } from "express";
+import { router } from "../router";
+import { port } from "../config";
+
+export const arion: Express = express();
+
+arion.use(express.json());
+arion.use(express.urlencoded({ extended: true }));
+arion.use(router); // application router
+
+// bootstrap
+export const bootstrap = async () => {
+  // ... upcoming invokes or callbacks
+  arion.listen(port, () =>
+    console.log(`🚀 Arion Server is online at http://localhost:${port}`)
+  );
+};
