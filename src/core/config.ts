b2d0a535ed27fc33e6cc1d35ab0759e070cf2b10
Mahabub Alam <mahabubx7@gmail.com>
2023-06-09 22:33:15 +0600

    :hammer: Fix(debug): fixed-env-string-parsing

--- src/core/config.ts
+++ src/core/config.ts
@@ -2,10 +2,15 @@ import { config } from "dotenv";
 
 config(); // invoking .env file parser
 
+interface ENV_VARIABLES {
+  port: number;
+  debug: boolean;
+}
+
 // register environment variable
-const env_variables = {
-  port: process.env.PORT, // application port
-  debug: process.env.DEBUG || false,
+const env_variables: ENV_VARIABLES = {
+  port: Number(process.env.PORT), // application port
+  debug: process.env.DEBUG === "true", // application debugging mode
 };
 
 // exporting all environment variables from the register
